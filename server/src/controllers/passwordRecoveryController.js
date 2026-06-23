6e7c948d650cde449b923aeb87bd7d3ae94ff2e4
JAGANKUMAR M <jagankumar0550@gmail.com>
2026-06-23 14:50:37 +0530

    Hardcode force IPv4 for Nodemailer

--- server/src/controllers/passwordRecoveryController.js
+++ server/src/controllers/passwordRecoveryController.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
+  family: 4, // Force IPv4 explicitly
   auth: {
     user: process.env.GMAIL_EMAIL,
     pass: process.env.GMAIL_APP_PASSWORD,
