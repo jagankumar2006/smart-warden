93f6bea5552d194a32254e79b278477318f0745e
JAGANKUMAR M <jagankumar0550@gmail.com>
2026-07-12 23:03:32 +0530

    Fix PROTOCOL_CONNECTION_LOST with auto-retry wrapper

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -6,7 +6,25 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+  keepAliveInitialDelay: 10000
 });
+
+// Auto-retry wrapper for connection lost errors
+const withRetry = (method) => {
+  return async (...args) => {
+    try {
+      return await method(...args);
+    } catch (error) {
+      if (error.code === 'PROTOCOL_CONNECTION_LOST' || error.code === 'ECONNRESET') {
+        console.warn('Database connection lost, retrying query...');
+        return await method(...args);
+      }
+      throw error;
+    }
+  };
+};
+
+pool.execute = withRetry(pool.execute.bind(pool));
+pool.query = withRetry(pool.query.bind(pool));
 
 module.exports = pool;
